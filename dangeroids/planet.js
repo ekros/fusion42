f8aa895213e110195f99ed7d805b515a78686eb4
eros <eros@acb.es>
2015-01-09 20:35:18 +0100

    - Adds draw function to Planet object - Rotation works well for any initial Alpha

--- dangeroids/planet.js
+++ dangeroids/planet.js
@@ -1,6 +1,6 @@
 // planet object definition
 
-function Planet(r, angle, speed, color)
+function Planet(r, angle, speed, color, size)
 {
 	var g = new PIXI.Graphics();
 
@@ -8,6 +8,18 @@ function Planet(r, angle, speed, color)
 	g.angle = angle;
 	g.speed = speed;
 	g.color = color;
+	g.size = size;
+	g.centerX = 0;
+	g.centerY = 0;
+
+	g.draw = function(x, y) {
+		g.centerX = x;
+		g.centerY = y;
+		g.beginFill(g.color);
+		g.drawCircle(x, y, g.size);
+		g.endFill();
+		stage.addChild(g); 	// add it the stage so we see it on our screens..
+	}
 
 	return g;
 }
